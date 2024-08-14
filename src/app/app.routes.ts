bb113d699bb4fd035244171f636c2902807e821c
David Hrdalo <davidhrdalo@gmail.com>
2024-08-15 08:09:58 +1000

    Routing for new components setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { GroupComponent } from './group/group.component';
+import { ChannelComponent } from './channel/channel.component';
+import { ChatComponent } from './chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:LoginComponent},
+    {path: 'account', component:AccountComponent},
+    {path: 'group', component:GroupComponent},
+    {path: 'channel', component:ChannelComponent},
+    {path: 'chat', component:ChatComponent},
+    { path: '', redirectTo: 'account', pathMatch: 'full' }, // Redirect to account if logged in
+];
