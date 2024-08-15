1006f9db788f8502f7bcfc00886e99b5efbe141e
David Hrdalo <davidhrdalo@gmail.com>
2024-08-15 17:27:21 +1000

    Renamed account to profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AccountComponent } from './account/account.component';
+import { ProfileComponent } from './profile/profile.component';
 import { GroupComponent } from './group/group.component';
 import { ChannelComponent } from './channel/channel.component';
 import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
     {path: 'login', component:LoginComponent},
-    {path: 'account', component:AccountComponent},
+    {path: 'profile', component:ProfileComponent},
     {path: 'group', component:GroupComponent},
     {path: 'channel', component:ChannelComponent},
     {path: 'chat', component:ChatComponent},
-    { path: '', redirectTo: 'account', pathMatch: 'full' }, // Redirect to account if logged in
+    { path: '', redirectTo: 'profile', pathMatch: 'full' }, // Redirect to profile if logged in
 ];
