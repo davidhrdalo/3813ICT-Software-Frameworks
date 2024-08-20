df2f66e687d99181d7049b91c88680f912e5e320
David Hrdalo <davidhrdalo@gmail.com>
2024-08-20 16:56:50 +1000

    Channel details now showing on channel template

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ChannelService } from '../services/channel/channel.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.css'
 })
-export class ChannelComponent {
+export class ChannelComponent implements OnInit {
+    channel: any = null;
 
+
+  constructor(
+    private route: ActivatedRoute,
+    private channelService: ChannelService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const groupId = +params.get('id')!;
+      const channelId = +params.get('channelId')!;
+      this.loadChannelDetails(groupId, channelId);
+    });
+  }
+
+  loadChannelDetails(groupId: number, channelId: number): void {
+    this.channelService.getChannelsByGroupId(groupId).subscribe(channels => {
+      this.channel = channels.find(channel => channel.id === channelId);
+      if (this.channel) {
+        console.log('Channel details:', this.channel); // Debugging log
+      } else {
+        console.log('Channel not found with ID:', channelId); // Debugging log
+      }
+    });
+  }
 }
