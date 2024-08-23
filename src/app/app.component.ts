910de82f47480a9da45c3ad04e3680248859095f
davidhrdalo <davidhrdalo@gmail.com>
2024-08-23 13:16:12 +1000

    Chat added to channel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { SocketService } from './services/socket/socket.service';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,11 @@ import { NavbarComponent } from "./navbar/navbar.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
-
   title = 'Yapper';
+
+  constructor(private socketService: SocketService) {
+    // Initialize your socket connection if needed
+    this.socketService.initSocket();
+  }
 
 }
--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -2,25 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChannelService } from '../services/channel/channel.service';
 import { CommonModule } from '@angular/common';
+import { SocketService } from '../services/socket/socket.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './channel.component.html',
-  styleUrl: './channel.component.css'
+  styleUrl: './channel.component.css',
 })
 export class ChannelComponent implements OnInit {
-    channel: any = null;
+  channel: any = null;
 
+  messagecontent: string = '';
+  messages: any[] = [];
+  ioConnection: any;
 
   constructor(
     private route: ActivatedRoute,
-    private channelService: ChannelService
+    private channelService: ChannelService,
+    private socketService: SocketService
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.initIoConnection();
+
+    this.route.paramMap.subscribe((params) => {
       const groupId = +params.get('id')!;
       const channelId = +params.get('channelId')!;
       this.loadChannelDetails(groupId, channelId);
@@ -28,13 +36,31 @@ export class ChannelComponent implements OnInit {
   }
 
   loadChannelDetails(groupId: number, channelId: number): void {
-    this.channelService.getChannelsByGroupId(groupId).subscribe(channels => {
-      this.channel = channels.find(channel => channel.id === channelId);
+    this.channelService.getChannelsByGroupId(groupId).subscribe((channels) => {
+      this.channel = channels.find((channel) => channel.id === channelId);
       if (this.channel) {
         console.log('Channel details:', this.channel); // Debugging log
       } else {
         console.log('Channel not found with ID:', channelId); // Debugging log
       }
     });
+  }
+
+  private initIoConnection() {
+    this.socketService.initSocket();
+    this.ioConnection = this.socketService
+      .getMessage()
+      .subscribe((message: any) => {
+        this.messages.push(message);
+      });
+  }
+
+  public chat() {
+    if (this.messagecontent) {
+      this.socketService.send(this.messagecontent);
+      this.messagecontent = '';
+    } else {
+      console.log('no message');
+    }
   }
 }
